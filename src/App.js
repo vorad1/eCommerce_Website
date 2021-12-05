669bafcd830200e78834a95926857fbbb43d19b0
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-12-06 05:57:34 +1300

    Added the addproducts page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./Components/Home"
-import  SignUp  from "./Components/SignUp"
-import Login from "./Components/Login"
-import  NotFound  from "./Components/NotFound"
-import AddProducts from './Components/AddProducts'
+import { Home } from './Components/Home'
+import { SignUp } from './Components/SignUp'
+import { Login } from './Components/Login'
+import { NotFound } from './Components/NotFound'
+import {AddProducts } from './Components/AddProducts'
+
 
 
 function App() {
@@ -13,11 +14,11 @@ function App() {
         {/* for routing */}
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path='/add-products' element={<AddProducts/>}/>
-            <Route element={<NotFound/>} />
+            <Route exact path="/" element={Home} />
+            <Route path="/signup" element={SignUp}/>
+            <Route path="/login" element={Login}/>
+            <Route path='/add-products' element={AddProducts} />
+            <Route element={NotFound} />
           </Routes>
         </BrowserRouter> 
     </div>
