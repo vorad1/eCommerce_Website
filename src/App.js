88e7c4db654512cead438c8cfe4f3fced6f0edc6
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-12-07 23:26:36 +1300

    Fixed compile time bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Components/Home'
-import Login from './Components/Login'
-import SignUp from './Components/SignUp'
-import NotFound from './Components/NotFound'
-import AddProducts from './Components/AddProducts'
-import Cart from './Components/Cart'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home } from './Components/Home'
+import { Login } from './Components/Login'
+import { Signup } from './Components/SignUp'
+import { NotFound } from './Components/NotFound'
+import { AddProducts } from './Components/AddProducts'
+import { Cart } from './Components/Cart'
 
 export const App = () => {
   return (
     <div>
       {/* for routing */}
       <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path='/add-products' element={<AddProducts />} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login } />
+          <Route path='/add-products' component={AddProducts } />
           <Route path="/cart" component={Cart} />
-          <Route element={<NotFound />} />
-        </Routes>
+          <Route component={NotFound } />
+        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/config/Config.js
+++ src/config/Config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_APP_API_KEY,
-  authDomain: NEXT_PUBLIC_APP_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_APP_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_APP_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_APP_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_APP_APP_ID,
-  measurementId: NEXT_PUBLIC_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyC20B__8Dk8JXoBmV99j48AGJKHrtQR6cE",
+  authDomain: "ecommerce-cc5ea.firebaseapp.com",
+  projectId: "ecommerce-cc5ea",
+  storageBucket: "ecommerce-cc5ea.appspot.com",
+  messagingSenderId: "827632597701",
+  appId: "1:827632597701:web:f05b7f718747a810710eac",
+  measurementId: "G-YX6JZHZTWV"
 };
 
 firebase.initializeApp(firebaseConfig);
