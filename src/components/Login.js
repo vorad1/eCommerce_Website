b038fcf3bcee0c341de38d57442ab5c297c35271
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-12-07 22:52:19 +1300

    Updated navbar and fixed the login page errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,91 +1,63 @@
-import React,{useState} from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../Config/Config";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { auth } from '../Config/Config'
+import { useHistory } from 'react-router-dom'
 
 export const Login = () => {
-    
-    const history = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const history = useHistory();
 
-    const [errorMsg, setErrorMsg] = useState("");
-    const [successMsg, setSuccessMsg] = useState("");
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [errorMsg, setErrorMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
 
     const handleLogin = (e) => {
         e.preventDefault();
         // console.log(email, password);
-        auth.signInWithEmailAndPassword(email,password)
-            .then(() => {
-                setSuccessMsg(
-                    "Login Successful. You will now automatically get redirected to Home page"
-                );
-                setEmail('');
-                setPassword('');
-                setErrorMsg('');
-                setTimeout(() => {
-                    setSuccessMsg('');
-                    history.push('/');
-                }, 3000);
-            })
-            .catch((error) => setErrorMsg(error.message));
-    };
+        auth.signInWithEmailAndPassword(email, password).then(() => {
+            setSuccessMsg('Login Successfull. You will now automatically get redirected to Home page');
+            setEmail('');
+            setPassword('');
+            setErrorMsg('');
+            setTimeout(() => {
+                setSuccessMsg('');
+                history.push('/');
+            }, 3000)
+        }).catch(error => setErrorMsg(error.message));
+    }
 
     return (
-        // Login Form
-        <div className="container">
+        <div className='container'>
             <br></br>
             <br></br>
             <h1>Login</h1>
             <hr></hr>
-            {successMsg && (
-                <>
-                    <div className="success-msg">{successMsg}</div>
-                    <br></br>
-                </>
-            )}
-            <form className="form-group" autoComplete="off" onSubmit={handleLogin}>
+            {successMsg && <>
+                <div className='success-msg'>{successMsg}</div>
+                <br></br>
+            </>}
+            <form className='form-group' autoComplete="off"
+                onSubmit={handleLogin}>
                 <label>Email</label>
-                <input
-                    type="email"
-                    className="form-control"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                ></input>
+                <input type="email" className='form-control' required
+                    onChange={(e) => setEmail(e.target.value)} value={email}></input>
                 <br></br>
                 <label>Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                ></input>
+                <input type="password" className='form-control' required
+                    onChange={(e) => setPassword(e.target.value)} value={password}></input>
                 <br></br>
-                {/* Redirect to sign up page if account doesn't exist*/}
-                <div className="btn-box">
-                    <span>
-                        Don't have an account SignUp
-                        <Link to="/signup" className="link">
-                            {" "}
-                            Here
-                        </Link>
-                    </span>
-                    <button type="submit" className="btn btn-success btn-md">
-                        LOGIN
-                    </button>
+                <div className='btn-box'>
+                    <span>Don't have an account SignUp
+                        <Link to="signup" className='link'> Here</Link></span>
+                    <button type="submit" className='btn btn-success btn-md'>LOGIN</button>
                 </div>
             </form>
-            {errorMsg && (
-                <>
-                    <br></br>
-                    <div className="error-msg">{errorMsg}</div>
-                </>
-            )}
+            {errorMsg && <>
+                <br></br>
+                <div className='error-msg'>{errorMsg}</div>
+            </>}
         </div>
-    );
-};
-
-
+    )
+}
